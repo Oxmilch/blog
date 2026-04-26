34a7dd4602873d1e646c88a0c2c4748c1e6dc272
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-04-26 17:20:10 +0900

    main:jsコメント追加

--- site/script/common.js
+++ site/script/common.js
@@ -1,10 +1,13 @@
 /** ■■■ クラス：リンク先を開く ■■■
  * @author Sozosha Recurrent School Fukuoka 2025116B T.H.
- * @since 20260425
+ * @since 20260426
  * @description
- * 複数あるHTMLファイルに対して、同じURLの設定や変更をしなくとも、JSで一括管理することにより、修正漏れや反映漏れを防ぐための仕組みとして実装しています
+ * (1) 複数あるHTMLファイルに対して、同じURLの設定や変更をしなくとも、JSで一括管理することにより、修正漏れや反映漏れを防ぐための仕組みとして実装しています
+ * (2) 今後変更がされない(Fix)前提であれば、こちらを使用せずにJS利用不可の場合でも動く、アンカーリンクタグによる実装の方が望ましいです。
+ * （３） 別サイトに遷移する際、当事業における分析ツール等の利用を想定し、"noreferrer" を意図的に設定していません。必要な場合は設定してください。
  */
 class PageOpen{
+    /* 特設サイト */
     /** 同じタブで開く：特設サイトトップページ */
     static eventTop() { window.location.href = "./index.html"; }
 
@@ -17,28 +20,61 @@ class PageOpen{
     /** 同じタブで開く：特設サイトTシャツ公募ページ */
     static eventPublicRecruitment() { window.location.href = "./public-recruitment.html"; }
 
-    /** 新規タブで開く：応募先フォームに遷移 */
+    /** 新規タブで開く：応募先フォーム */
     static eventForm(){ window.open("", "_blank", "noopener"); }
 
-    /** 新規タブで開く：事業専用インスタグラムへ遷移 */
+    /** 新規タブで開く：事業専用インスタグラム */
     static eventInstagram(){ window.open("", "_blank", "noopener"); }
 
-    /** 新規タブで開く：事業専用ティックトックに遷移 */
+    /** 新規タブで開く：事業専用ティックトック */
     static eventTiktok(){ window.open("", "_blank", "noopener"); }
 
-    /** 新規タブで開く：商工会議所トップ */
-    static fukuokaCCI(){ window.open("https://www.fukunet.or.jp/", "_blank", "noopener"); }
 
-    /** 新規タブで開く：商工会議所フェイスブック */
-    static fukuokaCCIFacebook(){ window.open("https://www.facebook.com/fukuoka.cci/", "_blank", "noopener"); }
+    /* 協力アーティスト情報 */
+    /** 新規タブで開く：7/25 協力アーティストインタグラム */
+    static eventCollaborator0725Instagram() { window.open("https://www.instagram.com/qp0123whyte/", "_blank", "noopener"); };
 
-    /** 新規タブで開く：商工会議所インスタグラム */
-    static fukuokaCCIInstagram(){ window.open("https://www.instagram.com/fukuoka_cci/", "_blank", "noopener"); }
+    /** 新規タブで開く：7/26 協力アーティストインタグラム */
+    static eventCollaborator0726Instagram() { window.open("https://www.instagram.com/colorhythmrisa/", "_blank", "noopener"); };
 
-    /** 新規タブで開く：商工会議所X */
-    static fukuokaCCIX(){ window.open("https://x.com/fukuoka_cci/", "_blank", "noopener"); }
+    /** 新規タブで開く：8/1 協力アーティストインタグラム */
+    static eventCollaborator0801Instagram() { window.open("https://www.instagram.com/yuriyoshinagaart/", "_blank", "noopener"); };
 
-    /** 新規タブで開く：福岡市美術館交通案内に遷移 */
+    /** 新規タブで開く：8/15 協力アーティストインタグラム */
+    static eventCollaborator0815Instagram() { window.open("https://www.instagram.com/chihiro_miyata/", "_blank", "noopener"); };
+
+    /** 新規タブで開く：8/16 協力アーティストインタグラム */
+    static eventCollaborator0816Instagram() { PageOpen.eventCollaborator0725Instagram(); };
+
+    /** 新規タブで開く：8/22 協力アーティストインタグラム */
+    static eventCollaborator0822Instagram() { window.open("https://www.instagram.com/fuguriddim/", "_blank", "noopener"); };
+
+    /** 新規タブで開く：8/30 協力アーティストインタグラム */
+    static eventCollaborator0830Instagram() { window.open("https://www.instagram.com/yutarokumabe/", "_blank", "noopener"); };
+
+
+    /* 主催情報 */
+    /** 新規タブで開く：福岡青年会議所トップ */
+    static fukuokaJC(){ window.open("https://www.fukuoka-jc.or.jp/", "_blank", "noopener"); }
+
+    /** 新規タブで開く：福岡青年会議所フェイスブック */
+    static fukuokaJCFacebook(){ window.open("https://www.facebook.com/jci.fukuoka/", "_blank", "noopener"); }
+
+    /** 新規タブで開く：福岡青年会議所インスタグラム */
+    static fukuokaJCInstagram(){ window.open("https://www.instagram.com/fukuokajc/", "_blank", "noopener"); }
+
+    /** 新規タブで開く：福岡青年会議所TikTok */
+    static fukuokaJCTikTok(){ window.open("https://www.tiktok.com/@fukuokajc", "_blank", "noopener"); }
+
+    /** 新規タブで開く：福岡青年会議所YouTube */
+    static fukuokaJCYoutube(){ window.open("https://www.youtube.com/channel/UCGsBcrSScCYV5_IvHejBXoA/", "_blank", "noopener"); }
+
+
+    /* 施設情報 */
+    /** 新規タブで開く：福岡市美術館 */
+    static fukuokaArtMusium(){ window.open("https://www.fukuoka-art-museum.jp/", "_blank", "noopener"); }
+
+    /** 新規タブで開く：福岡市美術館交通案内 */
     static fukuokaArtMusiumAccess(){ window.open("https://www.fukuoka-art-museum.jp/guide/access/", "_blank", "noopener"); }
 }
 
@@ -47,7 +83,10 @@ class PageOpen{
  * @author Sozosha Recurrent School Fukuoka 2025116B T.H.
  * @since 20260425
  * @description
- * 
+ * (1) 親要素のクラスが marquee の場合に動作します。
+ * (2) 厳密に言えばテキストだけではなく、要素自体をアニメーションさせているため、画像なども動かせます。
+ * (3) XとYの設定次第で、斜めや縦方向へのアニメーションも可能です。
+ * (4) 複雑なアニメーションはさせず、開始と終了の２点のキーフレームを結ぶ簡単なアニメーションのみ作成できます。
 */
 class MarqueeTexts{
     /** 動かす要素のIDやクラス名 */
@@ -103,4 +142,3 @@ class MarqueeTexts{
     /** アニメーション停止 */
     stop(){ this._marqueeElements.forEach((mqrqueeElement) => { mqrqueeElement.getAnimations()[0].pause();}); }
 }
-
