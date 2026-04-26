98b82696ba935ea9c2a4f397b7e83a06bc88f559
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-04-26 17:56:05 +0900

    main:pcレイアウトのマーキーテキストのチェック

--- site/script/index.js
+++ site/script/index.js
@@ -1,7 +1,16 @@
-const desktopMarqueeText1 = new MarqueeTexts(".test-marquee");
+const desktopMarqueeText1 = new MarqueeTexts(".test-marquee1");
+const desktopMarqueeText2 = new MarqueeTexts(".test-marquee2");
+const desktopMarqueeText3 = new MarqueeTexts(".test-marquee3");
+const desktopMarqueeText4 = new MarqueeTexts(".test-marquee4");
 
 /** ■■■ HTMLロード時実行処理 ■■■  */
 document.addEventListener("DOMContentLoaded", function(){
-    desktopMarqueeText1.set();
+    desktopMarqueeText1.set(12000, 0, "100vw", "-100vw", "10vh", "10vh");
+    desktopMarqueeText2.set(12000, 0, "100vw", "-200vw", "35vh", "35vh");
+    desktopMarqueeText3.set(12000, 0, "100vw", "-300vw", "60vh", "60vh");
+    desktopMarqueeText4.set(12000, 0, "100vw", "-400vw", "85vh", "85vh");
     desktopMarqueeText1.start();
+    desktopMarqueeText2.start();
+    desktopMarqueeText3.start();
+    desktopMarqueeText4.start();
 });
