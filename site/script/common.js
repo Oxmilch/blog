7e83b50ecb03f870b4130dc83a69597111a07a77
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-05-06 01:58:32 +0900

    main:ワークショップの作成作業途中をコミット

--- site/script/common.js
+++ site/script/common.js
@@ -9,3 +9,16 @@ class HamburgerMenu{
     /** 閉じる */
     static close(){ document.getElementById("hamburger-menu").close(); }
 }
+
+/** ■■■ クラス：アコーディオンメニュー ■■■
+ * @author Sozosha Recurrent School Fukuoka 2025.11~2026.05 Class 6B Team A - T.H.
+ * @since 20260506
+*/
+class AccordionMenu{
+    /** 開閉する
+     * @param {string} targetId 開閉対象のタグのId
+     */
+    static toggle(buttonObject, targetId){
+        document.getElementById(targetId).classList.toggle("open");
+    }
+}
