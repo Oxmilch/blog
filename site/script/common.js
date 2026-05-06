7fcb8c0fbf0b04d6e5bf61f3f4060ceffd4d99c0
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-05-06 18:35:20 +0900

    main: 作業途中コミット

--- site/script/common.js
+++ site/script/common.js
@@ -10,15 +10,34 @@ class HamburgerMenu{
     static close(){ document.getElementById("hamburger-menu").close(); }
 }
 
+
 /** ■■■ クラス：アコーディオンメニュー ■■■
  * @author Sozosha Recurrent School Fukuoka 2025.11~2026.05 Class 6B Team A - T.H.
  * @since 20260506
 */
 class AccordionMenu{
     /** 開閉する
-     * @param {string} targetId 開閉対象のタグのId
+     * @param {string} accordionMenuId 開閉対象のタグのId
      */
-    static toggle(targetId){
-        document.getElementById(targetId).classList.toggle("open");
+    static toggle(accordionMenuId){
+        document.getElementById(accordionMenuId).classList.toggle("open");
+    }
+
+    /** アコーディオンメニューを開くボタンを隠し、閉じるボタンを表示する
+     * @param {Element} openButtonElement 開くボタン
+     * @param {string} closeButtonId 表示する閉じるボタン
+     */
+    static openButton(openButtonElement, closeButtonId){
+        openButtonElement.classList.add("button-hidden");
+        document.getElementById(closeButtonId).classList.remove("button-hidden");
+    }
+
+    /** アコーディオンメニューを閉じるボタンを隠し、元のボタンを表示する
+     * @param {Element} closeButtonElement 閉じるボタン
+     * @param {string} openButtonId 表示する開くボタン
+     */
+    static closeButton(closeButtonElement, openButtonId){
+        closeButtonElement.classList.add("button-hidden");
+        document.getElementById(openButtonId).classList.remove("button-hidden");
     }
 }
