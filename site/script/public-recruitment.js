625362635eddb6972e39b2d4556be5f241e20850
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-05-15 02:29:40 +0900

    main: Tシャツアニメーション代替案

--- site/script/public-recruitment.js
+++ site/script/public-recruitment.js
@@ -1,13 +1,29 @@
-/** Tシャツ1枚目動作パラメータ */
-const gsapScParam1 = {};
-/** Tシャツ2枚目動作パラメータ */
-const gsapScParam2 = {};
-/** Tシャツ3枚目動作パラメータ */
-const gsapScParam3 = {};
+/** ■■■ クラス：Tシャツアニメーション ■■■
+ * @author Sozosha Recurrent School Fukuoka 2025.11~2026.05 Class 6B Team A - T.H.
+ * @since 20260515
+ */
+class TShirtAnimation{
+    constructor(){
+        // プラグインの読み込み
+        gsap.registerPlugin(ScrollTrigger);
+    }
+
+    // アニメーション実行
+    run(){
+        gsap.to("#how-apply-t-shirt-1", {
+            ease,
+            scrollTrigger: {
+                trigger: '.box',
+                start: 'center center'
+            }
+        })
+    }
+}
+
+
 
 /** DOMロード完了時イベント */
 document.addEventListener("DOMContentLoaded", function(event){
-    // プラグインの読み込み
-    gsap.registerPlugin(ScrollTrigger);
-
+    // const tshirt = new TShirtAnimation();
+    // tshirt.run();
 });
