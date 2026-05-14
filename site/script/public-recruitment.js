7bf52b7828a7bb6e9ee604ca8f2cb2846e150303
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-05-15 01:32:15 +0900

    main: Tシャツ公募 一通りのレイアウト完了、アニメーション用のライブラ入りを追加(gsap)

---
+++ site/script/public-recruitment.js
@@ -0,0 +1,13 @@
+/** Tシャツ1枚目動作パラメータ */
+const gsapScParam1 = {};
+/** Tシャツ2枚目動作パラメータ */
+const gsapScParam2 = {};
+/** Tシャツ3枚目動作パラメータ */
+const gsapScParam3 = {};
+
+/** DOMロード完了時イベント */
+document.addEventListener("DOMContentLoaded", function(event){
+    // プラグインの読み込み
+    gsap.registerPlugin(ScrollTrigger);
+
+});
